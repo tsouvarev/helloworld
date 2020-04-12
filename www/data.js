37e30a6364c51e6ffe46aad422fca4ff0100f803
Murad Byashimov <byashimov@gmail.com>
2020-04-12 21:59:34 +0300

    Update www/data.js

--- www/data.js
+++ www/data.js
@@ -5308,16 +5308,6 @@ const DATA={"weekendList": [
         "vendor": "teamtrip"
     },
     {
-        "end": "12.11.2020",
-        "level": 2,
-        "norm": "\u043f\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u0435 \u043f\u043e \u043c\u0430\u0440\u043e\u043a\u043a\u043e",
-        "start": "01.11.2020",
-        "tags": 16777748,
-        "title": "\u041f\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u0435 \u043f\u043e \u041c\u0430\u0440\u043e\u043a\u043a\u043e",
-        "url": "http://orangeked.ru/tours/item/25-puteshestvie-po-marokko",
-        "vendor": "orangeked"
-    },
-    {
         "end": "13.11.2020",
         "level": 2,
         "norm": "\u043f\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u0435 \u043f\u043e \u0441\u0435\u0432\u0435\u0440\u0443 \u0432\u044c\u0435\u0442\u043d\u0430\u043c\u0430",
@@ -5325,6 +5315,16 @@ const DATA={"weekendList": [
         "tags": 16777748,
         "title": "\u041f\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u0435 \u043f\u043e \u0421\u0435\u0432\u0435\u0440\u0443 \u0412\u044c\u0435\u0442\u043d\u0430\u043c\u0430",
         "url": "http://orangeked.ru/tours/item/74-vietnam",
+        "vendor": "orangeked"
+    },
+    {
+        "end": "12.11.2020",
+        "level": 2,
+        "norm": "\u043f\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u0435 \u043f\u043e \u043c\u0430\u0440\u043e\u043a\u043a\u043e",
+        "start": "01.11.2020",
+        "tags": 16777748,
+        "title": "\u041f\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u0435 \u043f\u043e \u041c\u0430\u0440\u043e\u043a\u043a\u043e",
+        "url": "http://orangeked.ru/tours/item/25-puteshestvie-po-marokko",
         "vendor": "orangeked"
     },
     {
@@ -5836,6 +5836,76 @@ const DATA={"weekendList": [
         "title": "\u041b\u044b\u0436\u043d\u044b\u0439 \u043f\u043e\u0445\u043e\u0434 \u043d\u0430 \u043f\u043b\u0430\u0442\u043e \u041c\u0430\u043d\u044c\u043f\u0443\u043f\u0443\u043d\u0435\u0440",
         "url": "https://cityescape.ru/lyzhnyj-poxod-na-plato-manpupunyor/",
         "vendor": "cityescape"
+    },
+    {
+        "end": "09.05.2021",
+        "level": 2,
+        "norm": "\u0444\u0440\u0438\u0434\u0430\u0439\u0432\u0438\u043d\u0433 \u0441\u043e \u0441\u0432\u043e\u0431\u043e\u0434\u043d\u044b\u043c\u0438 \u0434\u0435\u043b\u044c\u0444\u0438\u043d\u0430\u043c\u0438",
+        "start": "01.05.2021",
+        "tags": 262668,
+        "title": "\u0424\u0440\u0438\u0434\u0430\u0439\u0432\u0438\u043d\u0433 \u0441\u043e \u0441\u0432\u043e\u0431\u043e\u0434\u043d\u044b\u043c\u0438 \u0434\u0435\u043b\u044c\u0444\u0438\u043d\u0430\u043c\u0438",
+        "url": "https://turclub-pik.ru/pohod/fridaiving-s-delfinami-v-egipte/1973/",
+        "vendor": "pik"
+    },
+    {
+        "end": "09.05.2021",
+        "level": 8,
+        "norm": "\u043a\u0440\u044b\u043c \u043f\u043e \u0433\u043e\u0440\u0430\u043c \u0432\u0434\u043e\u043b\u044c \u043c\u043e\u0440\u044f",
+        "start": "01.05.2021",
+        "tags": 264204,
+        "title": "\u041a\u0440\u044b\u043c. \u041f\u043e \u0433\u043e\u0440\u0430\u043c \u0432\u0434\u043e\u043b\u044c \u043c\u043e\u0440\u044f",
+        "url": "https://turclub-pik.ru/pohod/po-goram-vdol-morya/1975/",
+        "vendor": "pik"
+    },
+    {
+        "end": "09.05.2021",
+        "level": 8,
+        "norm": "\u043f\u0435\u0448\u0438\u0439 \u043f\u043e\u0445\u043e\u0434 \u0438 \u0442\u0443\u0440 \u043d\u0430 \u043a\u0430\u044f\u043a\u0430\u0445 \u0432 \u043a\u0440\u044b\u043c\u0443",
+        "start": "01.05.2021",
+        "tags": 264204,
+        "title": "\u041f\u0435\u0448\u0438\u0439 \u043f\u043e\u0445\u043e\u0434 \u0438 \u0442\u0443\u0440 \u043d\u0430 \u043a\u0430\u044f\u043a\u0430\u0445 \u0432 \u041a\u0440\u044b\u043c\u0443",
+        "url": "https://turclub-pik.ru/pohod/krym-pohod-i-tur-na-kayakah/1979/",
+        "vendor": "pik"
+    },
+    {
+        "end": "11.05.2021",
+        "level": 8,
+        "norm": "\u043c\u0430\u043b\u044c\u0434\u0438\u0432\u044b \u0432\u0435\u0441\u0435\u043d\u043d\u0438\u0439 \u0441\u043f\u043e\u0440\u0442\u0438\u0432\u043d\u043e-\u043e\u0431\u0440\u0430\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044c\u043d\u044b\u0439 \u043b\u0430\u0433\u0435\u0440\u044c \u0434\u043b\u044f \u0432\u0441\u0435\u0439 \u0441\u0435\u043c\u044c\u0438",
+        "start": "01.05.2021",
+        "tags": 264453,
+        "title": "\u041c\u0430\u043b\u044c\u0434\u0438\u0432\u044b: \u0432\u0435\u0441\u0435\u043d\u043d\u0438\u0439 \u0441\u043f\u043e\u0440\u0442\u0438\u0432\u043d\u043e-\u043e\u0431\u0440\u0430\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044c\u043d\u044b\u0439 \u043b\u0430\u0433\u0435\u0440\u044c \u0434\u043b\u044f \u0432\u0441\u0435\u0439 \u0441\u0435\u043c\u044c\u0438",
+        "url": "https://team-trip.ru/maldivescamp_spr",
+        "vendor": "teamtrip"
+    },
+    {
+        "end": "11.05.2021",
+        "level": 8,
+        "norm": "\u043c\u0430\u043b\u044c\u0434\u0438\u0432\u044b \u0444\u0440\u0438\u0434\u0430\u0439\u0432\u0438\u043d\u0433 \u0439\u043e\u0433\u0430",
+        "start": "01.05.2021",
+        "tags": 264452,
+        "title": "\u041c\u0430\u043b\u044c\u0434\u0438\u0432\u044b: \u0444\u0440\u0438\u0434\u0430\u0439\u0432\u0438\u043d\u0433 & \u0439\u043e\u0433\u0430",
+        "url": "https://team-trip.ru/maldives_spr",
+        "vendor": "teamtrip"
+    },
+    {
+        "end": "11.05.2021",
+        "level": 8,
+        "norm": "\u044e\u0430\u0440 \u043a\u0435\u0439\u043f\u0442\u0430\u0443\u043d \u0438 \u043f\u0430\u0440\u043a \u043a\u0440\u044e\u0433\u0435\u0440",
+        "start": "01.05.2021",
+        "tags": 264452,
+        "title": "\u042e\u0410\u0420: \u041a\u0435\u0439\u043f\u0442\u0430\u0443\u043d \u0438 \u043f\u0430\u0440\u043a \u041a\u0440\u044e\u0433\u0435\u0440",
+        "url": "https://team-trip.ru/southafrica_cape",
+        "vendor": "teamtrip"
+    },
+    {
+        "end": "10.05.2021",
+        "level": 8,
+        "norm": "\u043a\u0440\u044b\u043c \u043f\u043e \u0433\u043e\u0440\u0430\u043c \u0432\u0434\u043e\u043b\u044c \u043c\u043e\u0440\u044f",
+        "start": "02.05.2021",
+        "tags": 264204,
+        "title": "\u041a\u0440\u044b\u043c. \u041f\u043e \u0433\u043e\u0440\u0430\u043c \u0432\u0434\u043e\u043b\u044c \u043c\u043e\u0440\u044f",
+        "url": "https://turclub-pik.ru/pohod/po-goram-vdol-morya/1976/",
+        "vendor": "pik"
     }
 ], "tagGroups": [
     {
